92e0dc062efdb6d58c9ff47ef59c7258d8533a64
Anujsharma002 <115893185+Anujsharma002@users.noreply.github.com>
2025-08-14 23:26:38 +0530

    final commit

--- Backend/RagUtils/Intializedata.py
+++ Backend/RagUtils/Intializedata.py
@@ -5,7 +5,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 import os
 import pandas as pd
-
+from sentence_transformers import SentenceTransformer
 csv_path = os.path.join(os.path.dirname(__file__), '..', 'employees_dataset.csv')
 df = pd.read_csv(os.path.abspath(csv_path))
 
@@ -15,16 +15,21 @@ docs = [
 ]
 text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500 ,chunk_overlap=100)
 splits = text_splitter.split_documents(docs)
+def setModelHugFaces():
+        model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2", cache_folder="./models")
+        model.save("./models/all-MiniLM-L6-v2")
 
 
 # add to vectordb
 def embeder():
-   embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+   embeddings = HuggingFaceEmbeddings(model_name="./models/all-MiniLM-L6-v2")
    return embeddings
 def save():
     vectorstore = FAISS.from_documents(splits, embeder())
     vectorstore.save_local("employeesEmbed")
     print("✅ Data stored in FAISS successfully!")
+    # model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2", cache_folder="./models")
+
 
 # results = vectorstore.similarity_search("Find employees skilled in Python", k=5)
 # for r in results:
@@ -32,3 +37,4 @@ def save():
 # print(results)
 # embeder()
 # save()
+# setModelHugFaces()
